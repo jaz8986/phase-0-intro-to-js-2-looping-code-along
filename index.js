b822d6372bc2af1d42c87a333c88a89296270288
Jaskiran Singh <jsingh8986@yahoo.com>
2022-12-01 15:41:17 -0800

    commit tree 9c11f280b39fda2a8ce73f637bdd3ab9919a22fb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) 
+//{[loop body]}
+
+let message = new Array()
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) { 
+        message[i]= `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+ } 
+  return message
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--)
+    }
+}
+
+countDown(10)
